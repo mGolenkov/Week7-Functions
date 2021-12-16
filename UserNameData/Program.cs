97ca9c093bfaf4694f7fb307891f4150858eb0f3
Maksim Golenkov <mgolenko@tktk.ee>
2021-12-16 23:40:04 +0200

    func

---
+++ UserNameData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UserNameData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada oma eesnimi
+            //programm kuvab kasutaja eesnime pikkust
+            //programm kuvab kasutaja eesnime esimest tähte
+            //programm kuvab kasutaja kasutaja eesnime tagurpidi
+            //main meetodis ei tohi olla rohkem, kui kolm rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = Console.ReadLine();
+
+            GetUserNameData(userName);
+        }
+        
+        public static void GetUserNameData(string UserInput)
+        {
+            Console.WriteLine($"Sinu nimi on {UserInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene täht on {UserInput[0]}.");
+
+            for (int i = UserInput.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(UserInput[i]);
+            }
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnimi
+            //programm küsib kasutajal sisestada numbrit 1 - 3
+            //kui kasutaja sisestab 1, siis kuvatakse kasutaja eesnimi tagurpidi
+            //kui kasutaja sisestab 2, siis kuvatakse kasutaja eesnimi esimest tähte
+            //kui kasutaja sisestab 3, siis kuvatakse kasutaja eesnimi pikkust
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta numbrit 1-3: ");
+            char userNumber = Convert.ToChar(Console.ReadLine());
+
+            switch (userNumber)
+            {
+                case '1':
+                    userChoiceOne(userName);
+                    break;
+                case '2':
+                    userChoiceTwo(userName);
+                    break;
+                case '3':
+                    userChoiceThree(userName);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+
+        }
+
+        private static void userChoiceThree(string userLength)
+        {
+            Console.WriteLine($"Sinu eesnime pikkus on {userLength.Length} sümbolit.");
+        }
+
+        private static void userChoiceTwo(string userLetter)
+        {
+            Console.WriteLine($"Sinu eesnimi esimene täht on {userLetter[0]}.");
+        }
+
+        private static void userChoiceOne(string userName)
+        {
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{userName[i]}");
+            }
+        }
+    }
+}
